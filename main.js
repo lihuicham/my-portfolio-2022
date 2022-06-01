fd46a7eae361f0572d85eadb4b4c6f782d09a41c
Li Hui <lihuicham@gmail.com>
2022-06-01 17:03:12 +0800

    Complete qualification section

--- main.js
+++ main.js
@@ -47,7 +47,25 @@ function toggleSkills() {
 skillsHeader.forEach(elem => elem.addEventListener('click', toggleSkills))
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => { 
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
