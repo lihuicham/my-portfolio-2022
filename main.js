fcd3d3202893071fbb2dee58b3c3c62585d5c9f0
Li Hui <lihuicham@gmail.com>
2022-06-01 14:31:47 +0800

    Complete skills section

--- main.js
+++ main.js
@@ -29,7 +29,22 @@ function linkAction() {
 navLink.forEach(link => link.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills_header')
 
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+
+    if(itemClass === 'skills_content skills_close') {
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach(elem => elem.addEventListener('click', toggleSkills))
 
 /*==================== QUALIFICATION TABS ====================*/
 
